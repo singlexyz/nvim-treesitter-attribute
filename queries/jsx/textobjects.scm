9ce3e8d5eb045f32d5934c82bda8b1ae7c64bcc6
singlexyz <singlexyz@gmail.com>
2024-01-28 02:47:11 +0800

    Inital.

---
+++ queries/jsx/textobjects.scm
@@ -0,0 +1,5 @@
+(jsx_attribute
+  (property_identifier)
+  [(string (_) @attribute.inner) (jsx_expression (_) @attribute.inner)]?
+) @attribute.outer
+(_) attribute: (jsx_expression) @attribute.outer
